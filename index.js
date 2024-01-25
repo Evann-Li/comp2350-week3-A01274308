bc3cbf70c949bcaf4ef5d5f5883a9246ba9e9f95
Evann Li <evannli450@gmail.com>
2024-01-25 13:49:57 -0800

    database info

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const port = process.env.PORT || 3000;
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfig = {
-	host: "host",
-	user: "user",
-	password: "password",
-	database: "database",
-	multipleStatements: false
+	host: "sql.freedb.tech",
+	user: "freedb_2350_main",
+	password: "SA33#V&vwb&!GUm",
+	database: "freedb_comp2350-week2-A01274308",
+	multipleStatements: false,
+	reconnect: true
 };
 
 var database = mysql.createPool(dbConfig);
